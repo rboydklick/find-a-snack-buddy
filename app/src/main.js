d320208f70560e2b5d5f9f1c90536afa4e53fe97
Riley Boyd <rboyd@klick.com>
2014-06-11 17:33:17 -0400

    now has one function, trying to get images in a column

--- app/src/main.js
+++ app/src/main.js
@@ -1,72 +1,60 @@
 /* globals define */
 define(function(require, exports, module) {
-//'use strict';
-// import dependencies
-var Engine = require('famous/core/Engine');
-var Modifier = require('famous/core/Modifier');
-var Transform = require('famous/core/Transform');
-var ImageSurface = require('famous/surfaces/ImageSurface');
-var Surface = require('famous/core/Surface');
-var Lightbox = require('famous/views/Lightbox');
+	//'use strict';
+	// import dependencies
+	var Engine = require('famous/core/Engine');
+	var Modifier = require('famous/core/Modifier');
+	var Transform = require('famous/core/Transform');
+	var ImageSurface = require('famous/surfaces/ImageSurface');
+	var Surface = require('famous/core/Surface');
+	var Lightbox = require('famous/views/Lightbox');
 
-var mainContext = Engine.createContext();
+	var mainContext = Engine.createContext();
 
-// An array with peoples' photos in it
-var peoplesPhotos = [];
-// index
-var index = 0;
+	// An array with peoples' photos in it
+	var peoplesPhotos = [];
+	// index
+	var index = 0;
 
-// Run functions
-imagesColumn();
-createLightbox();
+	// Run functions
+	imagesColumn();
 
-
-// Functions
-function imagesColumn() {
-	var imagesColumnContent = [
-	'<img src="content/images/doge150x150.png" />',
-	'<img src="content/images/doge150x150.png" />',
-	'<img src="content/images/doge150x150.png" />' 
-	];
+	// Functions
+	function imagesColumn() {
+		var imagesColumnContent = [
+		'<img src="content/images/famous_logo.png" />',
+		'<img src="content/images/doge150x150.png" />'
+		];
 
 
 
-	var background = new Surface({
-		properties: {
-			backgroundColor: '#000000'
-		}
-	});
-
-
-	mainContext.add(background);
-
-	for (var i = 0; i < imagesColumnContent.length; i++) {
-		var slide = new Surface({
-			content: imagesColumnContent[i],
+		var background = new Surface({
 			properties: {
-				color: 'white',
-				lineHeight: '200%',
-				textAlign: 'center',
-				fontSize: '36px',
-				cursor: 'pointer'
+				backgroundColor: '#000000'
 			}
 		});
 
-		peoplesPhotos.push(slide);
 
-} // End For loop
+		mainContext.add(background);
 
+		for (var i = 0; i < imagesColumnContent.length; i++) {
+			var slide = new Surface({
+				content: imagesColumnContent[i],
+				properties: {
+					color: 'white',
+					lineHeight: '200%',
+					textAlign: 'center',
+					fontSize: '36px',
+				}
+			});
 
+			peoplesPhotos.push(slide);
 
-} // End Images column function
+		} // End For loop
 
+		mainContext.add(slide);
 
-
-function createLightbox() {
-	lightbox = new Lightbox();
-	mainContext.add(lightbox);
-	lightbox.show(peoplesPhotos[0]);
-}
+	} // End Images column function
 
 });
 
